262c89982ae8fad8c42b122b6d5facf18337dfde
d-kurchenko <pvmnsd@gmail.com>
2024-08-16 16:59:27 +0300

    feat(hamster): add `merge away` & `twerk race` games support

--- src/mini-apps/entries/hamster/static.ts
+++ src/mini-apps/entries/hamster/static.ts
@@ -21,5 +21,7 @@ export const HamsterStatic = {
     'b4170868-cef0-424f-8eb9-be0622e8e8e3': 'd1690a07-3780-4068-810f-9b5bbf2931b2',
     'c4480ac7-e178-4973-8061-9ed5b2e17954': '82647f43-3f87-402d-88dd-09a90025313f',
     '43e35910-c168-4634-ad4f-52fd764a843f': 'd28721be-fd2d-4b45-869e-9f253b554e50',
+    'dc128d28-c45b-411c-98ff-ac7726fbaea4': '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+    '61308365-9d16-4040-8bb0-2f4a4c69074c': '61308365-9d16-4040-8bb0-2f4a4c69074c',
   } as { [Key in string]?: string },
 }
