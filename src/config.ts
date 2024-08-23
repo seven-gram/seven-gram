57e99939fd22aaaca393d2f792af3489a7b02ed1
d-kurchenko <pvmnsd@gmail.com>
2024-08-23 13:29:11 +0300

    feat(ping): show app version into message

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,14 @@
+import { readFileSync } from 'node:fs'
 import { memoize } from 'lodash-es'
+import type { PackageJson } from 'type-fest'
 import { useEnv } from './env.js'
 import { useConfigDatabase } from './config-database.js'
 
 export const useConfig = memoize(() => {
   const { NODE_ENV } = useEnv()
   const { database } = useConfigDatabase()
+
+  const packageJson: PackageJson = JSON.parse(readFileSync('package.json', { encoding: 'utf-8' }))
 
   const isLocal = NODE_ENV === 'local'
   const isProduction = NODE_ENV === 'production'
@@ -13,5 +17,6 @@ export const useConfig = memoize(() => {
     isLocal,
     isProduction,
     getComputedCommandPrefix: () => isLocal ? `${database.data.settings.commandPrefix}dev` : database.data.settings.commandPrefix,
+    packageJson,
   }
 })
--- src/modules/entries/ping.ts
+++ src/modules/entries/ping.ts
@@ -1,5 +1,8 @@
 import { AppMeta } from 'src/meta.js'
+import { useConfig } from 'src/config.js'
 import { defineModule } from '../helpers/define.js'
+
+const config = useConfig()
 
 export const pingModule = defineModule({
   type: 'command',
@@ -9,7 +12,8 @@ export const pingModule = defineModule({
     pattern: 'ping',
     description: `Replies ping message`,
     async handler({ event }) {
-      await event.message.reply({ message: `Hello from ${AppMeta.name}` })
+      const version = config.packageJson.version ? `v${config.packageJson.version}` : null
+      await event.message.reply({ message: `Hello from ${AppMeta.name} ${version}` })
     },
   },
 })
