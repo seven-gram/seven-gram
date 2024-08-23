0535a6c1f48caec6c8904fd0fc325ece0a1b2c25
d-kurchenko <pvmnsd@gmail.com>
2024-08-23 11:06:35 +0300

    feat: config-database automiggration

---
+++ src/config-database.ts
@@ -0,0 +1,52 @@
+import { existsSync, mkdirSync } from 'node:fs'
+import { JSONFileSyncPreset } from 'lowdb/node'
+import { memoize, merge } from 'lodash-es'
+
+interface Database {
+  settings: {
+    commandPrefix: string
+  }
+  userBot?: {
+    apiId: number
+    apiHash: string
+    sessionString: string
+    channels: Record<string, string | null>
+  }
+  bot?: {
+    token: string
+    username: string
+  } | null
+}
+
+export const useConfigDatabase = memoize(() => {
+  const dir = 'databases'
+
+  if (!existsSync(dir)) {
+    mkdirSync(dir, { recursive: true })
+  }
+
+  const defaultData: Database = {
+    settings: {
+      commandPrefix: '.',
+    },
+  }
+  const database = JSONFileSyncPreset<Database>(`${dir}/config.json`, defaultData)
+  database.data = merge(defaultData, database.data)
+  database.write()
+
+  const setUserBot = (userBot: Database['userBot']) => {
+    database.data.userBot = userBot
+    database.write()
+  }
+
+  const setBot = (bot: Database['bot'] | null) => {
+    database.data.bot = bot
+    database.write()
+  }
+
+  return {
+    database,
+    setUserBot,
+    setBot,
+  }
+})
