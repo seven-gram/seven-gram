0586e8a70b30f5395dc4095e0b8acd02e1bb6029
d-kurchenko <pvmnsd@gmail.com>
2024-09-29 14:29:10 +0300

    debug: change ping message

--- src/modules/entries/ping.ts
+++ src/modules/entries/ping.ts
@@ -1,5 +1,5 @@
-import { AppMeta } from 'src/meta.js'
 import { useConfig } from 'src/config.js'
+import { AppMeta } from 'src/meta.js'
 import { defineModule } from '../helpers/define.js'
 
 const config = useConfig()
@@ -14,7 +14,7 @@ export const pingModule = defineModule({
       description: `Replies ping message`,
       async handler({ event }) {
         const version = config.packageJson.version ? `v${config.packageJson.version}` : null
-        await event.message.reply({ message: `Hello from ${AppMeta.name} ${version}` })
+        await event.message.reply({ message: `Hello from ${AppMeta.name} ${version}!` })
       },
     },
   },
